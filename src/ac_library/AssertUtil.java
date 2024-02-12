efc7744441fbbb3fb85fb0e07267196b7dd70d9e
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-02-12 13:02:31 +0900

    [New] AssertUtil

---
+++ src/ac_library/AssertUtil.java
@@ -0,0 +1,25 @@
+package ac_library;
+
+final public class AssertUtil {
+    // if flag is false, throw AssertionError
+    public static void check(boolean flag) {
+        check(flag, "Check failed. flag is false.");
+    }
+    
+    // if flag is false, throw AssertionError with err_msg
+    public static void check(boolean flag, String err_msg) {
+        if(!flag) {
+            throw new AssertionError(err_msg);
+        }
+    }
+
+    // check l <= val && val < r
+    public static void checkRange(int val, int l, int r) {
+        check(l <= val && val < r, String.format("Check failed. Val must be %d <= val < %d. val = %d.", l, r, val));
+    }
+
+    // check 0 <= val && val < r
+    public static void checkRange(int val, int r) {
+        checkRange(val, 0, r);
+    }
+}
