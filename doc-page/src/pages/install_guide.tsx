50ea4f1a7075e3555327f337d7b10cd4071a09e2
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-02-20 13:25:04 +0900

    [Fix + Refactoring]

--- doc-page/src/pages/install_guide.tsx
+++ doc-page/src/pages/install_guide.tsx
@@ -1,15 +1,10 @@
-import Markdown from 'react-markdown'
+import Markdown from '@/components/Markdown'
 import fs from 'fs'
-import { GetStaticPaths, GetStaticProps } from "next";
-import path from 'path';
-import { Container } from 'react-bootstrap';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import remarkGfm from 'remark-gfm';
-import Head from 'next/head';
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { github } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
-import { ParsedUrlQuery } from 'querystring';
+import { GetStaticProps } from 'next'
+import path from 'path'
+import { Container } from 'react-bootstrap'
+import Head from 'next/head'
+import { ParsedUrlQuery } from 'querystring'
 
 export const Page = ({ content, slug }: { content: string, slug: string }) => {
   return (
@@ -17,32 +12,7 @@ export const Page = ({ content, slug }: { content: string, slug: string }) => {
         <Head>
           <title>{ `${slug} - ac library java` }</title>
         </Head>
-        <Markdown
-          className='markdown'
-          remarkPlugins={[remarkMath, remarkGfm]}
-          rehypePlugins={[rehypeKatex]}
-          components={{
-              code(props) {
-                const {children, className, node, ...rest} = props
-                const match = /language-(\w+)/.exec(className || '')
-                return match ? (
-                  <SyntaxHighlighter
-                    PreTag="div"
-                    language={match[1]}
-                    style={github}
-                  >
-                  {String(children).replace(/\n$/, '')}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code {...rest} className={className}>
-                    {children}
-                  </code>
-                )
-              }
-            }}
-        >
-            {content}
-        </Markdown>
+        <Markdown content={content} />
     </Container>
   )
 }
