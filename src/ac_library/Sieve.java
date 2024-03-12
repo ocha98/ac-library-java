d757d107d36ba233e757f5b0fe63292090897962
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-03-12 12:43:29 +0900

    [New] Sieve

---
+++ src/ac_library/Sieve.java
@@ -0,0 +1,98 @@
+package ac_library;
+
+public final class Sieve {
+    public static class PE {
+        public final int p, e;
+        public PE(int p, int e) {
+            this.p = p;
+            this.e = e;
+        }
+    }
+
+    private final int[] min_factor;
+    private final int n;
+    
+    public Sieve(int n) {
+        this.n = n;
+        min_factor = new int[n+1];
+        min_factor[1] = 1;
+
+        for (int i = 2;i <= n; ++i) {
+            if (min_factor[i] != 0) continue;
+            min_factor[i] = i;
+            final int m = n/i;
+            for (int j = i;j <= m; ++j) {
+                if (min_factor[i*j] == 0) {
+                    min_factor[i*j] = i;
+                }
+            }
+        }
+    }
+
+    public int[] getPrimes() {
+        if (n < 2) return new int[0];
+        int cnt = 1;
+        for (int i = 3;i <= n; i += 2) {
+            if(this.min_factor[i] == i)++cnt;
+        }
+
+        final int[] primes = new int[cnt];
+        primes[0] = 2;
+        int idx = 1;
+        for (int i = 3;i < n; i += 2) {
+            if (this.min_factor[i] == i) primes[idx++] = i;
+        }
+
+        return primes;
+    }
+
+    public boolean isPrime(int x) {
+        AssertUtil.check(0 <= x && x <= n);
+        if (x == 0 || x == 1) return false;
+        return min_factor[x] == x;
+    }
+
+    public java.util.ArrayList<PE> factorize(int x) {
+        AssertUtil.check(0 <= x && x <= n);
+        java.util.ArrayList<PE> res = new java.util.ArrayList<>();
+        while (x > 1) {
+            final int p = this.min_factor[x];
+            int e = 0;
+            while (this.min_factor[x] == p) {
+                x /= p;
+                ++e;
+            }
+            res.add(new PE(p, e));
+        }
+        return res;
+    }
+    
+    public int[] divisors(int x) {
+        AssertUtil.check(1 <= x && x <= n);
+        java.util.ArrayList<PE> factors = this.factorize(x);
+        int n = 1;
+        for (PE pe: factors) {
+            n *= pe.e + 1;
+        }
+        final int[] res = new int[n];
+        res[0] = 1;
+        int len = 1;
+        for (PE pe: factors) {
+            final int s = len;
+            final int p = pe.p;
+            final int e = pe.e;
+            for (int i = 0;i < s; ++i) {
+                int pow = 1;
+                final int resi = res[i];
+                for (int j = 0;j < e; ++j) {
+                    pow *= p;
+                    res[len++] = resi*pow;
+                }
+            }
+        }
+
+        return res;   
+    }
+}
+
+
